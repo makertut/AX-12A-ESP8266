ef30c43f75a636f01b634c850b19dbd6b894749d
jumejume1 <jumejume1@hotmail.com>
2020-04-02 11:57:02 +0700

    Create code1.ino

---
+++ code1.ino
@@ -0,0 +1,48 @@
+#include <AX12A.h>
+
+
+
+#define DirectionPin  (10u)
+
+#define BaudRate    (1000000ul)
+
+#define ID    (1u)
+
+
+
+void setup()
+
+{
+
+  
+
+ delay(1000); // Give time for Dynamixel to start on power-up 
+
+ ax12a.begin(BaudRate, DirectionPin, &Serial);
+
+ ax12a.setEndless(ID, ON);
+
+}
+
+void loop()
+
+{ 
+
+    ax12a.ledStatus(ID, ON);
+
+    ax12a.turn(ID, LEFT, 100);
+
+    delay(5000);
+
+  
+
+    ax12a.ledStatus(ID, OFF);
+
+    ax12a.turn(ID, RIGHT, 500);
+
+    delay(5000);
+
+    
+
+}
+
